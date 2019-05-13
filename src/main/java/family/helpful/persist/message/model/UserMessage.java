5783e762cd83fb04cd8e26ad777c145fea619f53
murat.doner <murat.doner@atg.se>
2019-05-13 15:18:24 +0200

    Fix in Channels

---
+++ src/main/java/family/helpful/persist/message/model/UserMessage.java
@@ -0,0 +1,16 @@
+package family.helpful.persist.message.model;
+
+
+import java.util.List;
+
+public class UserMessage {
+     List<User> userList;
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+}
